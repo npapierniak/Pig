e4dd3507e8806caf87341e54baeebcdb870866ec
Nicholas Papierniak <nick.papierniak@gmail.com>
2023-10-30 09:59:28 -0500

    Added game functionality

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -28,12 +28,19 @@ struct ContentView: View {
                 HStack{
                     Button("Roll")
                     {
-                        
+                        chooseRandom(times: 3)
+                        withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                            rotation += 360
+                        }
                     }
                     .buttonStyle(CustomButtonStyle())
                     Button("Hold)")
                     {
-                        
+                        gameScore += turnScore
+                        endTurn()
+                        withAnimation(.easeInOut(duration: 1)) {
+                            rotation += 360
+                        }
                     }
                     .buttonStyle(CustomButtonStyle())
                 }
@@ -44,6 +51,30 @@ struct ContentView: View {
             .padding()
         }
     }
+    
+    func endTurn()
+    {
+        turnScore = 0
+        randomValue = 0
+    }
+    func chooseRandom(times : Int){
+        if times < 0{
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+                randomValue = Int.random(in: 1...6)
+                chooseRandom(times: times - 1)
+            }
+        }
+        if times == 0{
+            if randomValue == 1{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+                    endTurn()
+                }
+            } else {
+                turnScore += randomValue
+            }
+        }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
