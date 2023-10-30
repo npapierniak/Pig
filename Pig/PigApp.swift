b17c17b47698f547db0695119803b812fbe6fa2c
Nicholas Papierniak <nick.papierniak@gmail.com>
2023-10-30 09:24:44 -0500

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Nicholas Papierniak on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
